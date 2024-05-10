6956e176f153514702501dfa911d639efe36c5f6
FrozenDev <125030768+FrozenQxc@users.noreply.github.com>
2024-05-10 17:08:22 +0300

    add some style

--- main/models.py
+++ main/models.py
@@ -3,19 +3,19 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Название")
        
     def __str__(self):
         return self.name
     
 class Blog(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='main/images')
-    url = models.URLField(blank=True)
-    date = models.DateField(default=timezone.now)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
-    categories = models.ManyToManyField(Category, related_name='blogs', blank=True)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(upload_to='main/images', verbose_name="Изображение")
+    url = models.URLField(blank=True, verbose_name="URL")
+    date = models.DateField(default=timezone.now, verbose_name="Дата")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None, verbose_name="Пользователь")
+    categories = models.ManyToManyField(Category, related_name='blogs', blank=True, verbose_name="Категории")
 
-    
+
 
